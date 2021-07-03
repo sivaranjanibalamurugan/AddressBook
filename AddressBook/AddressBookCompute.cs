c23e30cf362cd6b55cb4360e274a4875a095bf56
unknown <sivaranjanib99@gmail.com>
2021-07-03 22:30:33 +0530

    [siva] Adding new contacts

--- AddressBook/AddressBookCompute.cs
+++ AddressBook/AddressBookCompute.cs
@@ -8,10 +8,27 @@ namespace AddressBook
 {
     class AddressBookCompute
     {
+       
+        private LinkedList<ContactDetails> contactList;
+
+        public AddressBookCompute()
+        {
+            this.contactList = new LinkedList<ContactDetails>();
+        }
+
+        //this method add details to the address book
         public void AddContactDetails(string firstName, string lastName, string address, string city, string state, long zipCode, long phoneNumber)
         {
             ContactDetails contactDetails = new ContactDetails(firstName, lastName, address, city, state, zipCode, phoneNumber);
-            contactDetails.Display();
+            this.contactList.AddLast(contactDetails);
+        }
+
+        public void DisplayContact()
+        {
+            foreach (ContactDetails contact in this.contactList)
+            {
+                contact.Display();
+            }
         }
     }
 }
--- AddressBook/Operations.cs
+++ AddressBook/Operations.cs
@@ -10,33 +10,10 @@ namespace AddressBook
     {
         public static void ReadInput()
         {
-            Console.WriteLine("Enter the contact details");
-            AddDetails();
-
-        }
-
-        //gets the user details from the user
-        public static void AddDetails()
-        {
-            Console.WriteLine("Enter first Name");
-            string firstName = Console.ReadLine();
-            Console.WriteLine("Enter Last Name");
-            string lastName = Console.ReadLine();
-            Console.WriteLine("Enter Address");
-            string address = Console.ReadLine();
-            Console.WriteLine("Enter City");
-            string city = Console.ReadLine();
-            Console.WriteLine("Enter State");
-            string state = Console.ReadLine();
-            Console.WriteLine("Enter Zipcode");
-            long zipCode = Convert.ToInt64(Console.ReadLine());
-            Console.WriteLine("Enter Phone Number");
-            long phoneNumber = Convert.ToInt64(Console.ReadLine());
-
-            //create object for the class address compute and call the set contact detail method 
-            AddressBookCompute addressBookCompute = new AddressBookCompute();
-            addressBookCompute.AddContactDetails(firstName, lastName, address, city, state, zipCode, phoneNumber);
-            Console.Read();
+            AddressBookCompute AddressBookCompute = new AddressBookCompute();
+            AddressBookCompute.AddContactDetails("Siva", "Ranjani", "Nattukkal raja street","Rajapalayam", "Tamil Nadu",626117, 866736142);
+            AddressBookCompute.AddContactDetails("Rathna ", "Priyanga ", "Jeyalani street", "toothukodi", "Tamil Nadu", 625462, 9659048416);
+            AddressBookCompute.DisplayContact();
         }
     }
 }
